e156fc45a484b8957d7008a2b1ccc68e78da6207
kad0108 <kad0108@foxmail.com>
2016-09-26 19:03:46 +0800

    task17

---
+++ base.js
@@ -0,0 +1,13 @@
+function $(id){
+	return document.getElementById(id);
+}
+
+function addEvent(element, eventName, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, listener, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, listener);
+    } else {
+        element["on" + eventName] = listener;
+    }
+}
