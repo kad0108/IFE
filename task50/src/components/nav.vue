597ff4248d4e202a3bd45d5d7860338b87828891
kad0108 <kad0108@foxmail.com>
2016-12-04 17:08:49 +0800

    nav

--- task50/src/components/nav.vue
+++ task50/src/components/nav.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
-  	<span class="logo">问卷管理</span>
+  	<div class="logo"> 问卷管理</div>
+  	<a href="#/" class="home">我的问卷</a>
   </div>
 </template>
 
@@ -20,24 +21,41 @@ export default {
 	-webkit-user-select: none;
 	z-index: 1;
 	color: #fff;
-	height: 3.5rem;
+	height: 3rem;
 	background-color: #ee7419;
 	/* 弹性盒子子元素居中对齐 */
 	display: flex;
 	-webkit-align-items: center;
 }
 .logo{
+	display: inline-block;
 	margin-left: 10%;
-	font-size: 1.4rem;
+	font-size: 1.2rem;
 	display: inline-block;
 	cursor: default;
 }
 .logo:before{
+	display: inline-block;
 	content: '?';
-	width: 10px;
-	height: 10px;
+	width: 1.4rem;
+	height: 1.4rem;
 	background-color: #fff;
-	/*border-radius: 50%;*/
+	border-radius: 50%;
 	color: #ee7419;
+	text-align: center;
+	line-height: 1.5rem;
+}
+.home{
+	margin-left: 5%;
+	color: #fff;
+	font-size: 1rem;
+	text-decoration: none;
+	width: 6rem;
+	height: 100%;
+	line-height: 3rem;
+	text-align: center;
+}
+.home:hover{
+	background-color: rgb(255, 140, 25);
 }
 </style>
