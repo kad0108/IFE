c0175df9580494a2f42d185bf69fcc308c24fee8
kad0108 <kad0108@foxmail.com>
2017-02-26 18:09:14 +0800

    init

---
+++ util.js
@@ -0,0 +1,39 @@
+var EventUtil = {
+	addEvent: function(ele, eventName, listener){
+		if(ele.addEventListener){
+			ele.addEventListener(eventName, listener, false);		
+		}else if(ele.attachEvent){
+			ele.attachEvent("on" + eventName, listener);
+		}else{
+			ele["on" + eventName] = listener;
+		}
+	},
+	removeEvent: function(ele, eventName, listener){
+		if(ele.removeEventListener){
+			ele.removeEventListener(eventName, listener);		
+		}else if(ele.detachEvent){
+			ele.detachEvent("on" + eventName, listener);
+		}else{
+			ele["on" + eventName] = null;		
+		}
+	},
+	getEvent: function(e){
+		var evt = e || window.event;
+		return evt;
+	},
+	getTarget: function(e){
+		var e = this.getEvent(e);
+		var target = e.target || e.srcElement;
+		return target;
+	},
+	stopPropagation: function(e){
+		var e = this.getEvent(e);
+		if(e.stopPropagation) e.stopPropagation();
+		else e.cancelBubble = true;		
+	},
+	preventDefault: function(e){
+		var e = this.getEvent(e);
+		if(e.preventDefault) e.preventDefault();
+		else e.returnValue = false;//兼容firefox
+	}
+}
